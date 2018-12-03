29613a6976f6b43d1b28ea833ae5d99e252ba240
SheinaT <Sheina.tolmas@gmail.com>
2018-12-03 21:08:33 +0200

    first commit

---
+++ JS/task.js
@@ -0,0 +1,30 @@
+class Box extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+     
+  
+
+       
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="example">
+            </div>
+        );
+    }
+}
+
+function render() {
+    ReactDOM.render(
+        <App />,
+        document.getElementById("root")
+    );
+}
+
+render();
