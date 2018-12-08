2bd6c978de09665b813aa6526935319c7645ce2c
Guy Ginton <guyginton@gmail.com>
2018-12-08 15:47:29 +0200

    more modal udpates

--- JS/task.js
+++ JS/task.js
@@ -13,28 +13,44 @@ class App extends React.Component {
     }
 }
 
-// MODAL TO ALLOW TEXT EDITING. 
-// NEED TO ADD TO MAP ON MAP <BUTTON ONCLICK={THIS.ONEDITCLICK}> EDIT </BUTTON> TO PULL UP THIS MODAL
-// NEED TO ALSO ADD BUTTON TO MAP TO ALLOW DELETING MODAL
-// class ModalEditText extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.editItem = this.editItem.bind(this);
-//     editItem() {
-//         event.preventDefault();
-//         if (this.state.isEditing) {
-//         }
-//     }
-//     render() {
-//         return (
-//             <div className="editModal">
-//                 <form onSubmit={this.editItem}>
-//                     <input type="text" defaultValue={task} ref="editInput" />
-//                 </form>
-//             </div>
-//         );
-//     }
-// }
+class ModalEditText extends React.Component {
+    constructor(props) {
+        super(props);
+        // this.editItem = this.editItem.bind(this);
+    }
+    // editItem() {
+    //     event.preventDefault();
+    //     if (this.state.isEditing) {
+    //         var textInput = this.textInput.value;
+    //         var dateInput = this.dateInput.value;
+    //         var timeInput = this.timeInput.value;
+    //         var obj = {
+    //             textInput: textInput,
+    //             date: dateInput,
+    //             time: timeInput
+    //         }
+    //         this.props.input(obj);
+    //         this.counter += 1;
+    //         this.textInput.value = "";      //CLEAR AFTER SAVED IN OBJ
+    //         this.dateInput.value = "";
+    //         this.timeInput.value = "";
+    //     }
+    // }
+    render() {
+        return (
+            <div className="editModal">
+                <form onSubmit={this.editItem}>
+                    <input className="addNewItemText" ref={(input) => { this.textInput = input }} placeholder="Enter new item to your To Do list"></input>
+                    <input type="date" ref={(input) => { this.dateInput = input }}></input>
+                    <input type="time" ref={(input) => { this.timeInput = input }}></input>
+                    <br/>
+                    <button>Save</button>
+                    <button>Delete</button> 
+                </form>
+            </div>
+        );
+    }
+}
 
 class Menu extends React.Component {
     render() {
@@ -43,8 +59,8 @@ class Menu extends React.Component {
             <div className="wrapper">
                 <nav id="sidebar">
                     <div className="sidebar-header">
-                        <h3>Bootstrap Sidebar</h3>
-                        <img class="logo" src="./CSS/Images/task.png"></img>
+                        <h3>Get Shit Done</h3>
+                        <img className="logo" src="./CSS/Images/task.png"></img>
                     </div>
 
                     <div id="content">
@@ -84,7 +100,6 @@ class Main extends React.Component {
             categories: ["Main", "ITC", "Ulpan", "Chores"],
             toDoArray: [],
             completed: [],
-            isEditing: false
         };
     }
     toggleItem(event, list) {
@@ -180,6 +195,9 @@ class ToDoList extends React.Component {
         super(props);
         this.isChecked = this.isChecked.bind(this);
         this.editItem = this.editItem.bind(this);
+        this.state = {
+            isEditing: false
+        }
     }
     isChecked(event) {
         this.props.toggleItem(event, "toDoArray");
@@ -205,16 +223,21 @@ class ToDoList extends React.Component {
         this.render()
     }
     editItem(event) {
-        console.log(event.target.value)
+        this.setState({
+            isEditing: true
+        });
+        console.log(this.props.toDoProp[event.target.value].textInput)
     }
     render() {
+        var displayEditModal = this.state.isEditing ? <ModalEditText editItem={this.props.toDoProp[event.target.value]} /> : null;
         return (
             /////needs to pass an object////
             <div className="list">
-                <h3>To DO List: </h3>
+                <h3>To Do List: </h3>
                 <ul className="main-list">
                     {this.props.toDoProp.map((activity, i) => <li key={i} ><input type="checkbox" value={i} onChange={this.isChecked} checked={false} /> {this.generateItemString(activity)} <button onClick={this.editItem} className="edit-button" value={i}> EDIT </button> </li>)}
                 </ul>
+                {displayEditModal}
             </div>
         );
     }
@@ -256,7 +279,7 @@ class Done extends React.Component {
         return (
             /////needs to pass an object////
             <div className="completed-list">
-                <h3>Completed items:</h3>
+                <h3>Completed:</h3>
                 <ul className="done-list">
                     {this.props.doneProp.map((activity, j) => <li key={j} ><input type="checkbox" value={j} onChange={this.isChecked} checked={true} /> {this.generateDoneItemString(activity)}</li>)}
                 </ul>
